a10fe565690d4675b887f62d28c0dd84fbebf7a6
Kim <rodrigkim@gmail.com>
2022-08-29 17:55:36 -0400

    commit tree 88244b18a96c856fb8829912e6e59fadac5c1fe9

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(street){
+    return Math.abs(street - 42)
+}
+
+function distanceFromHqInFeet(street){
+    return 264 * distanceFromHqInBlocks(street)
+}
+
+function distanceTravelledInFeet(start, destination){
+    return 264 * Math.abs(destination - start)
+}
+
+function calculatesFarePrice(start, destination){
+    const distance = distanceTravelledInFeet(start, destination);
+    if (distance <= 400){
+        return 0
+    }
+    else if (2000 > distance){
+        return ((distance - 400) * 0.02)
+    }
+    else if (2500 > distance){
+        return 25
+    }
+    else {
+        return "cannot travel that far"
+    }
+}
+
